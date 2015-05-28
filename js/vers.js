d6711895a5dcaeeb158b8e1fc4a2dd977451b302
Seun Ogedengbe <seun40@gmail.com>
2015-05-28 16:38:05 -0500

    production version

---
+++ js/vers.js
@@ -0,0 +1,18 @@
+var vers_ix = {
+	status: 0,//alpha
+	major: 5,
+	minor: 0,
+	version: function() {return this.status.toString()+"."+this.major.toString()+"."+this.minor.toString();},
+    changelog: {
+        "0.5.0": "Initial Setup, Versioning",
+    },
+}
+var vers_wr = {
+	status: 0,//alpha
+	major: 5,
+	minor: 0,
+	version: function() {return this.status.toString()+"."+this.major.toString()+"."+this.minor.toString();},
+    changelog: {
+        "0.5.0": "Initial Setup, Versioning",
+    },
+}
