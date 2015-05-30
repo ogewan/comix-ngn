69327e7e2bce049392126fc12de0c84d59a9965a
Seun Ogedengbe <seun40@gmail.com>
2015-05-30 01:35:45 -0500

    Writer and Index updates
    Most of the updates were applied to Writer, adding form injection to
    configure pages/chapters
    License updated
    Changes included in vers

--- js/vers.js
+++ js/vers.js
@@ -1,20 +1,22 @@
 var vers_ix = {
 	status: 0,//alpha
 	major: 7,
-	minor: 7,
+	minor: 8,
 	version: function() {return this.status.toString()+"."+this.major.toString()+"."+this.minor.toString();},
     changelog: {
         "0.5.0": "Initial Setup, Versioning",
         "0.7.5": "Added Dependencies (JS): (jquery.min, angular-touch.min, angular.min[Update],bootstrap.min), Carousel for slides, real-time reactivity",
         "0.7.7": "Retooled carousel buttons, added page count checker",
+        "0.7.8": "Minor Modifications, page is aligned to center, index.html is cleaned up",
     },
 }
 var vers_wr = {
 	status: 0,//alpha
-	major: 5,
+	major: 8,
 	minor: 0,
 	version: function() {return this.status.toString()+"."+this.major.toString()+"."+this.minor.toString();},
     changelog: {
         "0.5.0": "Initial Setup, Versioning",
+        "0.8.0": "Huge implementations, Object config by dynamic form injection for Page and Chapter, Setting config compartmentalized, design config added",
     },
 }
