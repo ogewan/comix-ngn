c451551e1795804dc68093f1b576e0ee259db669
ogewan <seun40@gmail.com>
2015-09-03 22:18:15 -0500

    overwrite kill everything

--- ghs/lab/comixngn.js
+++ ghs/lab/comixngn.js
@@ -702,6 +702,7 @@ cG.stageInjection = function(SPECIFIC){
             }
             //console.log(sbvenue,nstpost);
             for(var z = 0;z<sbvenue.length;z++){
+                if(!cG.fBox.noverwrite) stages[iD].innerHTML = ""; //Kill everything
                 var sia = sbvenue[z].getAttribute("id"),
                     sua = sbvenue[z].getAttribute("use"),
                     sca = sbvenue[z].getAttribute("config");
