11a407826b4eab82ebe6790476b442aadca74bbe
ogewan <seun40@gmail.com>
2016-11-14 17:53:42 -0600

    Updated closure compiler; test compilation on mac

--- pcompile.py
+++ pcompile.py
@@ -4,9 +4,9 @@ import sys
 
 
 if sys.platform == 'darwin':
-    subprocess.check_call('java -jar ./factory/compiler.jar --js ./factory/def_libs.js ./factory/initialize.js ./factory/rollbar.js ./factory/retrieval.js ./factory/def_stage.js ./factory/def_stagechange.js ./factory/helpers.js ./factory/methods.js ./factory/start.js --create_source_map ./comixngn.js.map --js_output_file ./comixngn.min.js')
-    subprocess.check_call('rsync ./comixngn.min.js ./wordpress/comixngn/js/comixngn.min.js')
-    subprocess.check_call('zip -j -r foo foo ./wordpress/comixngn.zip ./wordpress/comixngn')
+    subprocess.check_call('java -jar ./factory/compiler.jar --js ./factory/def_libs.js ./factory/initialize.js ./factory/rollbar.js ./factory/retrieval.js ./factory/def_stage.js ./factory/def_stagechange.js ./factory/helpers.js ./factory/methods.js ./factory/start.js --create_source_map ./comixngn.js.map --js_output_file ./comixngn.min.js', shell=True)
+    subprocess.check_call('rsync ./comixngn.min.js ./wordpress/comixngn/js/comixngn.min.js', shell=True)
+    subprocess.check_call('zip -j ./wordpress/comixngn.zip ./wordpress/comixngn', shell=True)
 
 elif sys.platform == 'linux2':
     print("Linux")
