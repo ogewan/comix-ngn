f0192f22ed0768a3de5caa432c692c1e3ca3a486
ogewan <seun40@gmail.com>
2016-11-13 20:23:36 -0600

    pycompile added for cross compatibility need to test on mac

---
+++ pcompile.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import subprocess
+import sys
+
+
+if sys.platform == 'darwin':
+    subprocess.check_call('java -jar ./factory/compiler.jar --js ./factory/def_libs.js ./factory/initialize.js ./factory/rollbar.js ./factory/retrieval.js ./factory/def_stage.js ./factory/def_stagechange.js ./factory/helpers.js ./factory/methods.js ./factory/start.js --create_source_map ./comixngn.js.map --js_output_file ./comixngn.min.js')
+    subprocess.check_call('rsync ./comixngn.min.js ./wordpress/comixngn/js/comixngn.min.js')
+    subprocess.check_call('zip -j -r foo foo ./wordpress/comixngn.zip ./wordpress/comixngn')
+
+elif sys.platform == 'linux2':
+    print("Linux")
+elif sys.platform == 'win32':
+    subprocess.check_call('java -jar .\\factory\\compiler.jar --js .\\factory\\def_libs.js .\\factory\\initialize.js .\\factory\\rollbar.js .\\factory\\retrieval.js .\\factory\\def_stage.js .\\factory\\def_stagechange.js .\\factory\\helpers.js .\\factory\\methods.js .\\factory\\start.js --create_source_map .\\comixngn.js.map --js_output_file .\\comixngn.min.js')
+    subprocess.check_call('xcopy /y .\\comixngn.min.js .\\wordpress\\comixngn\\js\\comixngn.min.js')
+    subprocess.check_call('winrar a -u -r -ep1 -x*.zip -x*.bat .\\wordpress\\comixngn.zip .\\wordpress\\*.*')
