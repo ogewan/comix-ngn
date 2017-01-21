37a1bb25ee191e0e6e3cd8f3f2a679786e922ca2
ogewan <seun40@gmail.com>
2017-01-21 17:24:07 -0600

    fixed typo

--- pcompile.py
+++ pcompile.py
@@ -8,7 +8,7 @@ if sys.platform == 'darwin':
     subprocess.check_call('rsync ./comixngn.min.js ./wordpress/comixngn/js/comixngn.min.js', shell=True)
     subprocess.check_call('zip -j ./wordpress/comixngn.zip ./wordpress/comixngn', shell=True)
 
-elif sys.platform == 'linux2':
+elif sys.platform == 'linux':
     print("Linux")
 elif sys.platform == 'win32':
     subprocess.check_call('java -jar .\\factory\\compiler.jar --js .\\factory\\def_libs.js .\\factory\\initialize.js .\\factory\\rollbar.js .\\factory\\retrieval.js .\\factory\\def_stage.js .\\factory\\def_stagechange.js .\\factory\\helpers.js .\\factory\\methods.js .\\factory\\start.js --create_source_map .\\comixngn.js.map --js_output_file .\\comixngn.min.js')
