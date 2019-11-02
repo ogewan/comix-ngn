ac45ee1bab0d56a53de39cd3a2da76f1c390fc9d
ogewan <seun40@gmail.com>
2019-11-02 18:11:47 -0400

    chapter work

--- comixngn.ts
+++ comixngn.ts
@@ -5,7 +5,7 @@ console.log('comix-ngn v2');
 class Hexstring {
     value: number;
     toString = () => this.value.toString(16);
-    constructor(input: string|number) {
+    constructor(input: string | number) {
         if (typeof input === 'string') {
             this.value = parseInt(input, 16);
         } else {
@@ -31,7 +31,7 @@ class Page {
     animate?: boolean;
     permanent?: boolean;
     release?: number;
-    constructor(input: string|string[]|null, config?: any) {
+    constructor(input: string | string[] | null, config?: any) {
         let url;
         if (input) {
             if (typeof input === 'string') {
@@ -39,7 +39,7 @@ class Page {
             } else {
                 url = input;
             }
-            Object.assign(this, {...config, url});
+            Object.assign(this, { ...config, url });
         } else {
             Object.assign(this, config);
         }
@@ -56,15 +56,7 @@ class Page {
     }
 }
 class Chapter {
-    title?: string;
-    description?: string;
-    constructor(public start: number, public end: number) {
-        if (start > end) {
-            let tmp = start;
-            start = end;
-            end = tmp;
-        }
-    }
+    constructor(public start: number, public title?: string, public description?: string) { }
     toString() {
         return JSON.stringify(this);
     }
@@ -84,7 +76,7 @@ class Schema {
     loading = {
         diameter: 250,
         lines: 16,
-        rate: 1000/30,
+        rate: 1000 / 30,
         back: new Hexstring("#FFF"),
         color: new Hexstring("#373737")
     };
@@ -107,7 +99,7 @@ class Schema {
                 });
             }
             if (raw.chapters.length) {
-                raw.chapters = raw.chapters.map((e: any) => new Chapter(e.start, e.end));
+                raw.chapters = (<any[]>raw.chapters).map((e: any) => new Chapter(e.start, e.title, e.description)).sort((a: Chapter, b: Chapter) => a.start - b.start);
             }
             Object.assign(this, raw);
         } catch (e) {
@@ -140,14 +132,14 @@ class Comixngn {
     coreVersion = new Version(2, 0, 0);
     cxxVersion = new Version(0, 0, 2);
     bookMap: Map<string, CmxBook> = new Map();
-   /* get id () {
-        return this._id;
-    }
-    set id (_id: string) {
-        this._id = _id;
-    }*/
+    /* get id () {
+         return this._id;
+     }
+     set id (_id: string) {
+         this._id = _id;
+     }*/
     private sysmsg = `%c %c %c comix-ngn v${this.coreVersion} %c \u262F %c \u00A9 2020 Oluwaseun Ogedengbe %c`;
-    private sysclr = ["color:white; background:#2EB531", "background:purple","color:white; background:#32E237", 'color:red; background:black', "color:white; background:#2EB531", "color:white; background:purple"];
+    private sysclr = ["color:white; background:#2EB531", "background:purple", "color:white; background:#32E237", 'color:red; background:black', "color:white; background:#2EB531", "color:white; background:purple"];
 
     constructor() {
         console.log(this.sysmsg, ...this.sysclr);
@@ -164,7 +156,7 @@ class CmxBook extends HTMLElement {
     constructor() {
         super();
         this.core = comixngn();
-        const {core} = this;
+        const { core } = this;
 
         let j = 1;
         let uid = `STG${j}`;
@@ -175,7 +167,7 @@ class CmxBook extends HTMLElement {
         core.bookMap.set(uid, this);
 
         this._cid = window.location.host;
-        this.shadow = this.attachShadow({mode: 'open'});
+        this.shadow = this.attachShadow({ mode: 'open' });
 
         const schemaPath = this.getAttribute('schema');
         if (schemaPath) {
@@ -188,6 +180,9 @@ class CmxBook extends HTMLElement {
     private convertToDirectionSetting(data: Schema) {
 
     }
+    private pageChapterMap(chapterID?: number) {
+
+    }
     private initializeDisplay(data?: any) {
         // DIRECTION specific
         if (data) this._schema = new Schema(data);
@@ -195,14 +190,14 @@ class CmxBook extends HTMLElement {
         //call custom constructor
         const pages = this._schema ? this._schema.exportPages() : [];
         const settings = this._schema ? this.convertToDirectionSetting(this._schema) : {};
-        const base = new (<any>direction)(pages, {...settings, anchor: shadow});
+        const base = new (<any>direction)(pages, { ...settings, anchor: shadow });
         this.defineMethods(base);
         console.log('Intialize Display');
         const ctrlPath = this.getAttribute('controller');
         if (ctrlPath) {
             (<any>pegasus)(ctrlPath).then(this.initializeControls.bind(this));
         } else {
-            this.controller = <CmxCtrl> document.createElement('comix-ctrl');
+            this.controller = <CmxCtrl>document.createElement('comix-ctrl');
             this.insertAdjacentElement('afterend', this.controller);
         }
     }
@@ -214,7 +209,7 @@ class CmxBook extends HTMLElement {
     }
     private defineMethods(base: any) {
         // DIRECTION specific
-        const {pageToChapter, chapterToPage} = this;
+        const { pageToChapter, chapterToPage } = this;
         const chapterNavigation = (method: Function, to?: number) => {
             if (to !== 0 && !to) {
                 to = this.current() || 0;
@@ -235,21 +230,21 @@ class CmxBook extends HTMLElement {
         this.ch_prev = () => pageToChapter(this.go(chapterToPage((<number>this.ch_current()) - 1)));
         this.ch_next = () => pageToChapter(this.go(chapterToPage((<number>this.ch_current()) + 1)));
         this.ch_frst = () => pageToChapter(this.go(chapterToPage(0)));
-        this.ch_last = () => pageToChapter(this.go(chapterToPage(this._schema?this._schema.chapters.length: 0)));
+        this.ch_last = () => pageToChapter(this.go(chapterToPage(this._schema ? this._schema.chapters.length : 0)));
         this.update = () => {
-            const swap = <(arr: string[], opts: any, start?: number) => void> base.swap;
+            const swap = <(arr: string[], opts: any, start?: number) => void>base.swap;
             const pages = this._schema ? this._schema.exportPages() : [];
             const settings = this._schema ? this.convertToDirectionSetting(this._schema) : {};
-            swap(pages, {...settings, anchor: this.shadow});
+            swap(pages, { ...settings, anchor: this.shadow });
         };
         this.current = base.current;
-        this.ch_current = () => {pageToChapter(this.current())};
+        this.ch_current = () => { pageToChapter(this.current()) };
         this.rawData = base.data;
         this.pg_data = (to?: number) => {
             if (this._schema && this._schema.pages.length) {
                 return this._schema.pages[to || this.current() || 0];
             }
-        } 
+        }
         this.ch_data = (to?: number) => {
             if (this._schema && this._schema.chapters.length) {
                 return this._schema.chapters[to || this.ch_current() || 0];
@@ -295,23 +290,23 @@ class CmxBook extends HTMLElement {
     }
     _oldAttributeValue: any;
 
-    rand(): number|void {};
-    go(to?: number): number|void{};
-    prev(): number|void {};
-    next(): number|void {};
-    frst(): number|void {};
-    last(): number|void {};
-    ch_go(to?:number): number|void {};
-    ch_prev(): number|void {};
-    ch_next(): number|void {};
-    ch_frst(): number|void {};
-    ch_last(): number|void {};
-    update(): void{};
-    current(): number|void {};
-    ch_current(): number|void {};
-    rawData(to?: number): any|void {}
-    data(to?: number): Page|void {}
-    ch_data(to?: number): Chapter|void {}
+    rand(): number | void { };
+    go(to?: number): number | void { };
+    prev(): number | void { };
+    next(): number | void { };
+    frst(): number | void { };
+    last(): number | void { };
+    ch_go(to?: number): number | void { };
+    ch_prev(): number | void { };
+    ch_next(): number | void { };
+    ch_frst(): number | void { };
+    ch_last(): number | void { };
+    update(): void { };
+    current(): number | void { };
+    ch_current(): number | void { };
+    rawData(to?: number): any | void { }
+    data(to?: number): Page | void { }
+    ch_data(to?: number): Chapter | void { }
 }
 class CmxCtrl extends HTMLElement {
     constructor(public book: CmxBook) {
